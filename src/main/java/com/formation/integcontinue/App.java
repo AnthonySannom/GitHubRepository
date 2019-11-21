0a1294295b7628cc81197e1ff3c680bedcca2829
Anthony S <antsila@hotmail.com>
2019-11-21 10:41:37 +0100

    initialasation du depot

---
+++ src/main/java/com/formation/integcontinue/App.java
@@ -0,0 +1,22 @@
+package com.formation.integcontinue;
+
+/**
+ * Hello world! Ajout de modification
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World! test sans erreur");
+		
+		int mor = 10;
+		int mor2 = 1;
+		int j;
+		boolean inutile = true;
+		
+		for (int i = 0; i < 10; i++) {
+			j = mor + mor2;
+			
+			
+		}
+System.out.println("NUL");
+	}
+}
---
+++ src/main/java/com/formation/integcontinue/Test.java
@@ -0,0 +1,8 @@
+package com.formation.integcontinue;
+
+public class Test {
+	public Test() {
+		System.out.println("Bonjour");
+	}
+
+}
